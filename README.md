2f79e7855ab62eb0313e1a708d60b1d43f6d2740
oEnzoCastro <enzocastru@gmail.com>
2025-05-11 16:40:47 -0300

    CREATE: REFORMA

--- README.md
+++ README.md
@@ -25,6 +25,10 @@ The workspace contains two folders by default, where:
 
 Meanwhile, the compiled output files will be generated in the `bin` folder by default.
 
+##
+
+Version: 1.0.0
+
 ## Dependency Management
 
 The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
--- src/App.java
+++ src/App.java
@@ -1,127 +1,7 @@
-import DAO.DAO_InvertedList;
-import java.io.IOException;
-import java.util.Scanner;
-import services.CRUD_BTree;
-import services.CRUD_Hash;
-import services.Sorting;
-
 public class App {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
-        boolean isRunning = true;
-        Scanner scan = new Scanner(System.in);
-        String fileTree = "src/database/billionairesTree.db";
-        String fileHash = "src/database/billionairesHash.db";
-
-        while (isRunning) {
-            printMenu();
-            int option = scan.nextInt();
-            int id;
-            int algoritmo;
-            String key;
-
-            switch (option) {
-                case 1: // Create All
-                    algoritmo = selectAlgorithm(scan);
-                    if (algoritmo == 1) {
-                        CRUD_BTree.createAll();
-                    } else if (algoritmo == 2) {
-                        DAO_InvertedList.limparLista();
-                        CRUD_Hash.createAll();
-                    }
-                    clearScreen();
-                    break;
-
-                case 2: // Create
-                    algoritmo = selectAlgorithm(scan);
-                    if (algoritmo == 1) {
-                        CRUD_BTree.create(fileTree);
-                    } else if (algoritmo == 2) {
-                        CRUD_Hash.create(fileHash);
-                    }
-                    break;
-
-                case 3: // Read
-                    algoritmo = selectAlgorithm(scan);
-                    System.out.print("Chave: ");
-                    clearInputBuffer(scan);
-                    id = scan.nextInt();
-                    clearScreen();
-                    if (algoritmo == 1) {
-                        CRUD_BTree.getIndex(id);
-                    } else if (algoritmo == 2) {
-                        CRUD_Hash.getIndex(id);
-                    }
-                    break;
-
-                case 4: // Update
-                    algoritmo = selectAlgorithm(scan);
-                    System.out.print("Chave: ");
-                    clearInputBuffer(scan);
-                    key = scan.nextLine();
-                    clearScreen();
-                    if (algoritmo == 1) {
-                        CRUD_BTree.update(key, fileTree);
-                    } else if (algoritmo == 2) {
-                        CRUD_Hash.update(key, fileHash);
-                    }
-                    break;
-
-                case 5: // Delete
-
-                    algoritmo = selectAlgorithm(scan);
-                    System.out.print("ID: ");
-                    clearInputBuffer(scan);
-                    id = scan.nextInt();
-                    clearScreen();
-                    if (algoritmo == 1)
-                        CRUD_BTree.delete(id, fileTree);
-                    else if (algoritmo == 2) {
-                        CRUD_Hash.delete(id, fileHash);
-                        DAO_InvertedList.deleteIL(id);
-                        DAO_InvertedList.deleteIL(id);
-                    }
-
-                    break;
-
-                case 6: // Sort
-                    clearScreen();
-                    System.out.print("Registros: ");
-                    int registros = scan.nextInt();
-                    clearInputBuffer(scan);
-                    System.out.print("Caminhos: ");
-                    int caminhos = scan.nextInt();
-                    Sorting.sort(fileTree, registros, caminhos);
-                    Sorting.sort(fileHash, registros, caminhos);
-                    break;
-
-                case 7: // Lista Invertida
-                    clearScreen();
-                    scan.nextLine();
-                    System.out.print("Palavra a ser pesquisada: ");
-                    String palavra = scan.nextLine();
-                    System.out.println("Escolha a lista a ser pesquisada:");
-                    System.out.println("1. Para coluna Source");
-                    System.out.println("2. Para coluna Country");
-                    int code = scan.nextInt();
-                    DAO_InvertedList.searchIL(palavra, code);
-                    break;
-
-                case 8:
-                    clearScreen();
-                    System.out.println("Programa Encerrado!");
-                    isRunning = false;
-                    break;
-
-                default:
-                    clearScreen();
-                    System.err.println("Opção Inválida");
-                    break;
-            }
-        }
-
-        scan.close();
     }
 
     public static void printMenu() {
@@ -139,31 +19,23 @@ public class App {
         System.out.println("----------------------------------");
     }
 
-    public static int selectAlgorithm(Scanner scan) {
-        System.out.println("----------------------------------");
-        System.out.println("Escolha qual algoritmo: ");
-        System.out.println("1 -> Árvore B");
-        System.out.println("2 -> Tabela Hash");
-        int choice = scan.nextInt();
-        switch (choice) {
-            case 1:
-                return choice;
-            case 2:
-                return choice;
+    // public static int selectAlgorithm(Scanner scan) {
+    //     System.out.println("----------------------------------");
+    //     System.out.println("Escolha qual algoritmo: ");
+    //     System.out.println("1 -> Árvore B");
+    //     System.out.println("2 -> Tabela Hash");
+    //     int choice = scan.nextInt();
+    //     switch (choice) {
+    //         case 1:
+    //             return choice;
+    //         case 2:
+    //             return choice;
 
-            default:
-                System.out.println("----------------------------------");
-                System.out.println("Opção Inválida!");
-                return selectAlgorithm(scan);
-        }
-    }
+    //         default:
+    //             System.out.println("----------------------------------");
+    //             System.out.println("Opção Inválida!");
+    //             return selectAlgorithm(scan);
+    //     }
+    // }
 
-    public static void clearInputBuffer(Scanner scan) {
-        scan.nextLine(); // Clear buffer
-    }
-
-    public static void clearScreen() {
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
-    }
 }
