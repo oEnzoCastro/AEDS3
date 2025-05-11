a5349b5031431faf7dc42ddb7f2bed6264624d85
oEnzoCastro <enzocastru@gmail.com>
2025-05-11 18:32:23 -0300

    ADD: New Menu

--- src/App.java
+++ src/App.java
@@ -1,41 +1,61 @@
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
 
+        // *.new()
+
+        // Start
+
+        switch (printMenu()) {
+            case 1: // Create All
+
+                break;
+
+            case 2: // Create One
+
+                break;
+
+            default:
+                break;
+        }
+
+        // End
+
+        // *.close()
+
     }
 
-    public static void printMenu() {
-        System.out.println("----------------------------------");
-        System.out.println("Select your options:");
-        System.out.println("----------------------------------");
-        System.out.println("1. Create from CSV");
-        System.out.println("2. Create");
-        System.out.println("3. Read");
-        System.out.println("4. Update");
-        System.out.println("5. Delete");
-        System.out.println("6. Sort");
-        System.out.println("7. Procurar Palavra nas Listas Invertidas");
-        System.out.println("8. Exit");
-        System.out.println("----------------------------------");
+    public static int printMenu() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("|————————————————————————————————————————————|");
+        System.out.println("|            Select your option:             |");
+        System.out.println("|———|————————————————————————————————————————|");
+        System.out.println("| 1 | Create from CSV                        |");
+        System.out.println("|———|————————————————————————————————————————|");
+        System.out.println("| 2 | Create                                 |");
+        System.out.println("|———|————————————————————————————————————————|");
+        System.out.println("| 3 | Read                                   |");
+        System.out.println("|———|————————————————————————————————————————|");
+        System.out.println("| 4 | Update                                 |");
+        System.out.println("|———|————————————————————————————————————————|");
+        System.out.println("| 5 | Delete                                 |");
+        System.out.println("|———|————————————————————————————————————————|");
+        System.out.println("| 6 | Sort                                   |");
+        System.out.println("|———|————————————————————————————————————————|");
+        System.out.println("| 7 | Procurar Palavra nas Listas Invertidas |");
+        System.out.println("|———|————————————————————————————————————————|");
+        System.out.println("| 8 | Exit                                   |");
+        System.out.println("|———|————————————————————————————————————————|");
+        System.out.print("| Opção: ");
+        int res = scanner.nextInt();
+        System.out.println("|————————————————————————————————————————————|");
+        scanner.close();
+
+        return res;
+
     }
-
-    // public static int selectAlgorithm(Scanner scan) {
-    //     System.out.println("----------------------------------");
-    //     System.out.println("Escolha qual algoritmo: ");
-    //     System.out.println("1 -> Árvore B");
-    //     System.out.println("2 -> Tabela Hash");
-    //     int choice = scan.nextInt();
-    //     switch (choice) {
-    //         case 1:
-    //             return choice;
-    //         case 2:
-    //             return choice;
-
-    //         default:
-    //             System.out.println("----------------------------------");
-    //             System.out.println("Opção Inválida!");
-    //             return selectAlgorithm(scan);
-    //     }
-    // }
-
 }
